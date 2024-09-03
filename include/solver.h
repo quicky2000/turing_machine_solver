f336612d65991b1cf27645779764c363526e9af1
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2024-09-03 18:14:22 +0200

    Define some checkers and register them, solver can select them

--- include/solver.h
+++ include/solver.h
@@ -18,24 +18,164 @@
 #ifndef TURING_MACHINE_SOLVER_SOLVER_H
 #define TURING_MACHINE_SOLVER_SOLVER_H
 
+#include "checker_base.h"
+#include "quicky_exception.h"
+#include <map>
+#include <memory>
+#include <iostream>
+
 namespace turing_machine_solver
 {
     class solver
     {
     public:
-        inline
-        solver(unsigned int p_nb_verifiers);
+        inline explicit
+        solver(unsigned int p_nb_checkers);
+
+        inline static
+        std::shared_ptr<checker_if>
+        get_checker(unsigned int p_id);
 
     private:
-        unsigned int m_nb_verifiers;
+
+        inline static
+        void
+        register_all_checkers();
+
+        inline static
+        void
+        register_checker(const std::shared_ptr<checker_if> & p_checker);
+
+        unsigned int m_nb_checkers;
+
+        std::vector<std::shared_ptr<checker_if>> m_checkers;
+
+        inline static std::map<unsigned int, std::shared_ptr<checker_if>> m_all_checkers;
     };
 
     //-------------------------------------------------------------------------
-    solver::solver(unsigned int p_nb_verifiers)
-    :m_nb_verifiers{p_nb_verifiers}
+    solver::solver(unsigned int p_nb_checkers)
+    :m_nb_checkers{p_nb_checkers}
     {
-
+        if(m_all_checkers.empty())
+        {
+            register_all_checkers();
+        }
+        do
+        {
+            for(auto l_iter: m_all_checkers)
+            {
+                std::cout << l_iter.first << " " << l_iter.second->get_name() << std::endl;
+            }
+            unsigned int l_id;
+            std::cin >> l_id;
+            m_checkers.emplace_back(get_checker(l_id));
+        } while (m_checkers.size() < p_nb_checkers);
     }
+
+    //-------------------------------------------------------------------------
+    void
+    solver::register_all_checkers()
+    {
+        register_checker(std::shared_ptr<checker_if>
+                         {new checker_base<3>
+                          (2
+                          ,"Le chiffre du triangle bleu comparé à 3"
+                          ,{checker_func{[](const candidate &p_candidate) -> bool
+                                         {return p_candidate.get_blue_triangle() < 3;}
+                                        ,"bleu < 3"
+                                        }
+                           ,{[](const candidate &p_candidate) -> bool
+                             {return p_candidate.get_blue_triangle() == 3;}
+                            ,"bleu == 3"
+                            }
+                           ,{[](const candidate &p_candidate) -> bool
+                             {return p_candidate.get_blue_triangle() > 3;}
+                            , "bleu > à 3"
+                            }
+                           }
+                          )
+                         }
+                        );
+        register_checker(std::shared_ptr<checker_if>
+                         {new checker_base<3>
+                          (3
+                          ,"Le chiffre du carre jaune comparé à 3"
+                          ,{checker_func{[](const candidate &p_candidate) -> bool
+                                         {return p_candidate.get_yellow_square() < 3;}
+                                        ,"jaune < 3"
+                                        }
+                           ,{[](const candidate &p_candidate) -> bool
+                             {return p_candidate.get_yellow_square() == 3;}
+                            ,"jaune == 3"
+                            }
+                           ,{[](const candidate &p_candidate) -> bool
+                             {return p_candidate.get_yellow_square() > 3;}
+                             , "jaune > à 3"
+                            }
+                           }
+                          )
+                         }
+                        );
+        register_checker(std::shared_ptr<checker_if>
+                         {new checker_base<3>
+                          (4
+                          ,"Le chiffre du carré jaune comparé à  4"
+                          ,{checker_func{[](const candidate &p_candidate) -> bool
+                                         {return p_candidate.get_yellow_square() < 4;}
+                                        ,"jaune < 4"
+                                        }
+                           ,{[](const candidate &p_candidate) -> bool
+                             {return p_candidate.get_yellow_square() == 4;}
+                            ,"jaune == 4"
+                            }
+                           ,{[](const candidate &p_candidate) -> bool
+                             {return p_candidate.get_yellow_square() > 4;}
+                             , "jaune > à 4"
+                            }
+                           }
+                          )
+                         }
+                        );
+        register_checker(std::shared_ptr<checker_if>
+                         {new checker_base<2>
+                          (7
+                          ,"cercle violet pair ou impair"
+                          ,{checker_func{[](const candidate &p_candidate) -> bool
+                                         {return !(p_candidate.get_purple_circle() % 2);}
+                                        ,"violet est pair"
+                                        }
+                           ,{[](const candidate &p_candidate) -> bool
+                             {return (p_candidate.get_purple_circle() % 2);}
+                            ,"violet est impair"
+                            }
+                           }
+                          )
+                         }
+                        );
+    }
+
+    //-------------------------------------------------------------------------
+    std::shared_ptr<checker_if>
+    solver::get_checker(unsigned int p_id)
+    {
+        auto l_iter = m_all_checkers.find(p_id);
+        if(m_all_checkers.end() == l_iter)
+        {
+            throw quicky_exception::quicky_logic_exception("No checker with ID " + std::to_string(p_id), __LINE__, __FILE__);
+        }
+        return l_iter->second;
+    }
+
+    //-------------------------------------------------------------------------
+    void
+    solver::register_checker(const std::shared_ptr<checker_if> & p_checker)
+    {
+        assert(!m_all_checkers.contains(p_checker->get_id()));
+        m_all_checkers.insert(make_pair(p_checker->get_id(), p_checker));
+    }
+
+
 }
 #endif //TURING_MACHINE_SOLVER_SOLVER_H
 // EOF
